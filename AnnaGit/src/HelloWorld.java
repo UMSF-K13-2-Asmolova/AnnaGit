975302458d318e363343d67d9514b9062a442fa1
UMSF-K13-2-Asmolova <ancha-asmolova@yandex.ru>
2016-05-30 16:37:37 +0300

    Commit2

--- AnnaGit/src/HelloWorld.java
+++ AnnaGit/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 
-System.out.println("Hello World");
+System.out.println("Hello WOrld");
 for(int i=0;i<10;i++)
 System.out.println(i);
 
